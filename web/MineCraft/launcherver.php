b62775e8fcff8fbc3115791bf8fc1906ee7d23d5
«serega6531» <serega6531@yandex.ru>
2014-01-01 16:10:06 +0400

    Just update launcher version script

--- web/MineCraft/launcherver.php
+++ web/MineCraft/launcherver.php
@@ -1,10 +1,9 @@
 <?php
-
-//WORKING ON WEBMCR
-//IF YOU DONT HAVE WEBMCR JUST DO
-//die('1'); //WHERE 1 IS LAUNCHER VERSION
-
 require('../system.php');
 BDConnect('launcherver');
-echo sqlConfigGet('launcher-version');
+if ($_GET['what'] == 'version') {
+	echo sqlConfigGet('launcher-version');
+} else {
+	echo '00000000000000000000000000000000';
+}
 ?>
